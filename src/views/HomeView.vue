6c7991e96361f0f80b3664364adc2c8825b2b38a
luongductrong <luongductrong2004@gmail.com>
2025-01-22 16:29:12 +0700

    Update: install lucide-vue-next; Remove: unnecessary files

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,7 @@
 <!-- @format -->
 
-<script setup lang="ts">
-  import TheWelcome from '../components/TheWelcome.vue';
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <main></main>
 </template>
