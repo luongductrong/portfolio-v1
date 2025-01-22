2f9de8351318e07c8f712997f3351b69bb2ab6dd
luongductrong <luongductrong2004@gmail.com>
2025-01-23 02:05:20 +0700

    Create: SectionAbout, SectionBlog component.

---
+++ src/components/SectionAbout.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <p class="bg-gray-800 p-4 rounded-lg">
+    Passionate developer with a love for web development and technology. Always learning and sharing knowledge with the
+    community.
+  </p>
+</template>
