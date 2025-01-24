4e8e1b9335bd127eb7180278e5e64a1d029465e5
luongductrong <luongductrong2004@gmail.com>
2025-01-24 15:30:32 +0700

    Update: tailwind config, index.html: use darkMode html class; App.vue: background: add darkmode

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
 </script>
 
 <template>
-  <div class="min-h-screen bg-gray-900 text-gray-300 p-6 font-mono">
+  <div class="min-h-screen bg-gray-50 dark:bg-gray-900 text-gray-300 p-6 font-mono">
     <div class="max-w-screen-xl mx-auto">
       <AppHeader />
       <AppNavigator />
